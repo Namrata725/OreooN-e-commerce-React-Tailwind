2686500beb74edf20c91a3bc001f4dc79386fdef
namrata <namrataap725@gmail.com>
2025-10-29 20:59:31 +0545

    added testinonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,11 +1,119 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 import Heading from "./Heading";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+import Customer1 from "../assets/customer1.jpg";
+import Customer2 from "../assets/customer2.jpg";
+import Customer3 from "../assets/customer3.jpg";
+import Customer4 from "../assets/customer4.jpg";
+import Customer5 from "../assets/customer5.jpg";
+import { FaStar } from "react-icons/fa";
 
 function Testimonials() {
+  const reviews = [
+    {
+      id: 1,
+      name: "Emily Johnson",
+      profession: "Food Blogger",
+      rating: 5,
+      para: "FreshBasket is my go-to store for all grocery needs. Their produce is always fresh, and the delivery is super fast. I love the user-friendly interface and variety of organic options!",
+
+      image: Customer1,
+    },
+
+    {
+      id: 2,
+      name: "David Smith",
+      profession: "Chef",
+      rating: 4,
+      para: "As a chef, quality ingredients are everything. FreshBasket consistently delivers the best vegetables, herbs, and pantry staples. Highly recommended!",
+      image: Customer2,
+    },
+
+    {
+      id: 3,
+      name: "Alya Zahra",
+      profession: "Model",
+      rating: 3,
+      para: "Shopping online with FreshBasket has saved me so much time. I trust them for my family's weekly groceries—always fresh, affordable, and reliable.",
+      image: Customer3,
+    },
+
+    {
+      id: 4,
+      name: "Carlos Mendes",
+      profession: "Fitness Coach",
+      rating: 5,
+      para: "I appreciate the selection of healthy foods and clean-label products. FreshBasket has been a great partner in my wellness journey!",
+      image: Customer4,
+    },
+
+    {
+      id: 5,
+      name: "Natcha Phongchai",
+      profession: "Nutritionist",
+      rating: 4,
+      para: "FreshBasket offers high-quality groceries at reasonable prices. Their organic section is impressive, and their customer service is top-notch.",
+      image: Customer5,
+    },
+  ];
   return (
     <section>
       <div className="max-w-[1400px] px-10 mx-auto py-20">
         <Heading orange="Customers" normal="Testimonials" />
+        <div className="py-5 flex justify-end gap-x-3">
+          <button className="custom-prev text-2xl text-zinc-800 rounded-lg size-11 flex bg-zinc-100 justify-center items-center hover:bg-gradient-to-b hover:from-orange-400 hover:to-orange-500 hover:text-white cursor-pointer">
+            <IoIosArrowBack />
+          </button>
+          <button className="custom-next text-2xl text-zinc-800 rounded-lg size-11 flex bg-zinc-100 justify-center items-center hover:bg-gradient-to-b hover:from-orange-400 hover:to-orange-500 hover:text-white cursor-pointer">
+            <IoIosArrowForward />
+          </button>
+        </div>
+
+        <Swiper
+          navigation={{ nextEl: ".custom-next", prevEl: ".custom-prev" }}
+          modules={[Navigation]}
+          className="mySwiper"
+          loop={true}
+          breakpoints={{
+            640: { slidesPerView: 1, spaceBetween: 20 },
+            768: { slidesPerView: 2, spaceBetween: 20 },
+            1024: { slidesPerView: 3, spaceBetween: 20 },
+          }}
+        >
+          {reviews.map((review) => {
+            return (
+              <SwiperSlide className="bg-zinc-100 rounded-xl p-8">
+                <div className="flex gap-5 items-center">
+                  <div className="size-16 rounded-full bg-red-500 outline-2 outline-orange-500 outline-offset-4 overflow-hidden">
+                    <img
+                      src={review.image}
+                      alt={review.name}
+                      className="size-full "
+                    />
+                  </div>
+                  <div>
+                    <h5 className="text-xl font-bold">{review.name}</h5>
+                    <p className="text-zinc-600 ">{review.profession}</p>
+                    <span className="flex text-yellow-400 mt-2 text-xl gap-1">
+                      {Array.from({ length: review.rating }, (_, i) => (
+                        <FaStar />
+                      ))}
+                    </span>
+                  </div>
+                </div>
+
+                <div className="mt-10 min-h-[15vh]">
+                  <p className="text-zinc-600">{review.para}</p>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </section>
   );
